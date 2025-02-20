ee65503d635e3b297e1626326a996727e9eb465c
Sanaasadad <sanaasadad4@gmail.com>
2025-02-20 12:02:22 +0100

    ajouter deux fonction crée et afficher

--- src/main/java/com/brief/DAO/CoursDao.java
+++ src/main/java/com/brief/DAO/CoursDao.java
@@ -3,22 +3,39 @@ package com.brief.DAO;
 
 
 import com.brief.Model.Cours;
+import com.mysql.cj.x.protobuf.MysqlxCrud;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoursDao {
 
-    public void addCourse(Cours course){
-
+    public void createCours(Cours cours) throws SQLException {
+        String sql = "INSERT INTO  courses (nom_cours, descrption) VALUES (?, ?)";
+        try (Connection conn = DbConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, cours.getNom_cours());
+            stmt.setString(2, cours.getDescrption());
+        }
     }
 
-    //public List<Cours> getAllCourses() {  }
+    public List<Cours> getAll() throws SQLException {
+        List<Cours> courses = new ArrayList<>();
+        String sql = "SELECT * FROM courses";
+        try (Connection conn = DbConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Cours cours = new Cours();
+                cours.setNom_cours(rs.getString("nom_cours"));
+                cours.setDescrption(rs.getString("descrption"));
+                courses.add(cours);
+            }
+        }
 
-    public void updateCourse(Cours course) { }
-    public void deleteCourse(Cours course) { }
 
-
-
+        return courses;
+    }
 
 }
-
--- src/main/java/com/brief/Model/Cours.java
+++ src/main/java/com/brief/Model/Cours.java
@@ -5,12 +5,12 @@ package com.brief.Model;
 public class Cours {
     private int id;
     private String nom_cours;
-    private String date_naissance;
+    private String descrption;
 
-    public Cours(int id, String nom_cours, String date_naissance) {
+    public Cours(int id, String nom_cours, String descrption) {
         this.id = id;
         this.nom_cours = nom_cours;
-        this.date_naissance = date_naissance;
+        this.descrption = descrption;
     }
 
     public Cours() {
@@ -32,11 +32,11 @@ public class Cours {
         this.nom_cours = nom_cours;
     }
 
-    public String getDate_naissance() {
-        return date_naissance;
+    public String getDescrption() {
+        return descrption;
     }
 
-    public void setDate_naissance(String date_naissance) {
-        this.date_naissance = date_naissance;
+    public void setDescrption(String descrption) {
+        this.descrption = descrption;
     }
 }
